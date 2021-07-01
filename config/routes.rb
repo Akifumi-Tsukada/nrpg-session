fc2f1532c7c5f10059fda0cae137da0f19359639
Tsukada-Akifumi <a.tsuka0190@gmail.com>
2021-07-01 10:08:12 +0900

    トップページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "rooms#index"
+  get "rooms/index"
+  root to: 'toppage#index'
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
